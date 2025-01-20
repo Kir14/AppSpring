7517a92bc9ceb4291ab967705b7dc5041090ccc4
Kirill <62447478+Kir14@users.noreply.github.com>
2025-01-20 11:04:47 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public void printHello() {
+        System.out.println("Hello World");
+    }
+
+}
